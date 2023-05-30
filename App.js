ba1f8e1ab237e7f1cc9fc9465c59963758581bbb
Makktu <johnmcna@gmail.com>
2023-05-30 20:37:04 +0100

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style='inverted' />
+      <View style={styles.container}>
+        <TouchableOpacity>
+          <Text style={styles.textStyle}>On Guard</Text>
+          {/* <Text style={styles.altTextStyle}>created by John McNamara</Text> */}
+
+          <Text style={styles.startStyle}>Tap To Begin</Text>
+        </TouchableOpacity>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#0E043D',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textStyle: {
+    color: 'orangered',
+    fontSize: 50,
+  },
+  altTextStyle: {
+    color: 'orangered',
+    fontSize: 10,
+  },
+  startStyle: {
+    color: 'whitesmoke',
+    fontSize: 30,
+    marginVertical: 50,
   },
 });
---
+++ README.md
@@ -0,0 +1,8 @@
+# SentinelApp
+
+First draft of a phone app that will provide "dead man's switch"-style personal security.
+
+- One the app is 'armed', the user must keep a hardware button pressed.
+- If the user releases the button, the device immediately locks and sounds a siren at maximum volume.
+- Only the user, by unlocking the phone, can disable the siren.
+- Any thief or attacker cannot disable the siren, or lower its volume.
